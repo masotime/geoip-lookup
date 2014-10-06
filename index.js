34245df1dadbc5e67f93e27c97036ae2ec403d1b
Benjamin Goh <bengoh@paypal.com>
2014-10-06 12:15:55 -0700

    Fix annoying blocking issue

--- index.js
+++ index.js
@@ -14,18 +14,22 @@ var db = levelup('./iplookup', {
 module.exports = {
 	'lookup': function(ip, callback) {
 		db.get(ip, function(err, value) {
-			var session;
 			if (err) {
 				if (err.notFound) {
-					// try and find it, so that future calls will have access
-					session = scraper.session();
-					session.getJson('https://www.maxmind.com/geoip/v2.1/city/'+ip+'?demo=1')
-						.then(function(json) {
-							return Q.ninvoke(db, 'put', ip, json);
-						}).catch(function(err) {
-							console.error('Could not get location info for '+ip, err, err.stack);
-						});
-					return callback(); // just undefined
+					// don't block
+					callback();
+					setImmediate(function() {
+						// try and find it, so that future calls will have access
+						var session;
+						session = scraper.session();
+						session.getJson('https://www.maxmind.com/geoip/v2.1/city/'+ip+'?demo=1')
+							.then(function(json) {
+								return Q.ninvoke(db, 'put', ip, json);
+							}).catch(function(err) {
+								console.error('Could not get location info for '+ip, err, err.stack);
+							});
+					});
+					return;
 				} else {
 					return callback(err);	
 				}
